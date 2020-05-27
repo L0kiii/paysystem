7ebc78f57c719db53593fda70d6a87529544e9f6
L0kiii <572607501@qq.com>
2020-05-27 21:15:10 +0800

    create_pay_system

---
+++ src/test/java/com/l0ki/paysystem/PaysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l0ki.paysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
